34d1a2ebd900226d1134f25ba5be5a74005c6523
RenatoArenas <renato.arenas2012@gmail.com>
2022-01-14 10:38:57 -0500

    Agregado botón para ejecutar función

--- js/number.js
+++ js/number.js
@@ -1,18 +1,22 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7 , 8, 9];
+let numbers = [1, 2, 3, 4, 5, 6, 7 , 8, 9];
+let numberSaved = [];
 
 function randomNumbers(numbers) {
     for (let i = numbers.length - 1; i >= 0; i--) {
-        let randomNumber = Math.floor(Math.random() * numbers.length);
-        console.log(randomNumber)
-        return numbers.splice(randomNumber, 1);
+        return Math.floor(Math.random() * numbers.length);
+        
     }
 }
 
 function placeNumbers() {
     for (let i = 0; i < 9; i++) {
         const numberContainer = document.getElementById(i)
+        randomNumbers(numbers)
+        let number = numbers.splice(randomNumbers(numbers), 1);
+        numberContainer.textContent = number[0];
         
-        numberContainer.textContent = randomNumbers(numbers);
+        numberSaved.unshift(number[0]);
     }
+    numbers = numberSaved
 }
 placeNumbers();
